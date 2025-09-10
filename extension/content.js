104814d3f05e46ed1237edf6eb8fb01da26d962f
felprangel <feliperangel897@gmail.com>
2025-09-10 16:11:06 -0300

    standardize quote usage in message event listener

--- extension/content.js
+++ extension/content.js
@@ -5,24 +5,24 @@ function notifyExtension() {
             ZebraPrintingExtensionId: chrome.runtime.id,
             ZebraPrintingVersion: chrome.runtime.getManifest().version,
         },
-        "*"
+        '*'
     );
 }
 
 notifyExtension();
 
 // Listen to messages from the current website
-window.addEventListener("message", function (event) {
-    if (typeof event.data.type === "undefined") {
+window.addEventListener('message', function (event) {
+    if (typeof event.data.type === 'undefined') {
         return;
     }
 
-    if (event.data.type == "zebra_print_label") {
+    if (event.data.type == 'zebra_print_label') {
         chrome.runtime.sendMessage(event.data);
         return;
     }
 
-    if (event.data.type == "zebra_ping") {
+    if (event.data.type == 'zebra_ping') {
         notifyExtension();
         return;
     }
